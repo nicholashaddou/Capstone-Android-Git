fafaaa46948aba4877c64a491072681afd45064d
Lukmanul Hakim <jurpro.online@gmail.com>
2022-05-21 20:56:32 +0700

    Mempersiapkan arsitektur file
    Mempersiapkan arsitektur file
    Menambahkan activity Welcome

---
+++ app/src/main/java/com/c22ps175/playlab/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.c22ps175.playlab.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c22ps175.playlab.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
