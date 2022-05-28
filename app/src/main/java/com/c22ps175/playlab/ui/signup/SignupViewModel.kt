f7fb06db6a3a20d88f9b916fd28ba2c6aa6f1124
Lukmanul Hakim <jurpro.online@gmail.com>
2022-05-28 21:19:45 +0700

    Memperbaiki import package
    Memperbaiki import package dari contoh picodiploma menjadi project capstone

--- app/src/main/java/com/c22ps175/playlab/ui/signup/SignupViewModel.kt
+++ app/src/main/java/com/c22ps175/playlab/ui/signup/SignupViewModel.kt
@@ -2,8 +2,8 @@ package com.c22ps175.playlab.ui.signup
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dicoding.picodiploma.loginwithanimation.model.UserModel
-import com.dicoding.picodiploma.loginwithanimation.model.UserPreference
+import com.c22ps175.playlab.ui.model.UserModel
+import com.c22ps175.playlab.ui.model.UserPreference
 import kotlinx.coroutines.launch
 
 class SignupViewModel(private val pref: UserPreference) : ViewModel() {
