2cddb779057746fc8f0c9eb4ed1426ca0822e767
Nicholas Haddou <54536702+nicholashaddou@users.noreply.github.com>
2022-05-29 19:36:47 +0700

    Menambahkan Course list serta dashboard, sudah ada template tinggal otak atik sesuai course

---
+++ app/src/main/java/com/c22ps175/playlab/ui/course/CourseListActivity.kt
@@ -0,0 +1,12 @@
+package com.c22ps175.playlab.ui.course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c22ps175.playlab.R
+
+class CourseListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_list)
+    }
+}
---
+++ app/src/main/java/com/c22ps175/playlab/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.c22ps175.playlab.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c22ps175.playlab.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
