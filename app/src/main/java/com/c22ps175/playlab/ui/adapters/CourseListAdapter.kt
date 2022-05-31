2ed4236f53fe0dcf4daf80a0c1b3b52ae728422d
Nicholas Haddou <54536702+nicholashaddou@users.noreply.github.com>
2022-05-31 10:37:44 +0700

    Menambahkan adapter, baca comment di CourseListAdapter.kt

---
+++ app/src/main/java/com/c22ps175/playlab/ui/adapters/CourseListAdapter.kt
@@ -0,0 +1,58 @@
+package com.c22ps175.playlab.ui.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.c22ps175.playlab.data.CourseData
+import com.c22ps175.playlab.databinding.ItemRowCourseListBinding
+
+class CourseListAdapter : RecyclerView.Adapter<CourseListAdapter.ViewHolder>() {
+
+    private val list = ArrayList<CourseData>()
+    private var onItemClickCallback: OnItemClickCallback? = null
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+    class ViewHolder(private val binding: ItemRowCourseListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(courseData: CourseData) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(courseData.courseImageURL)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .circleCrop()
+                    .into(ivItemPhoto)
+                tvUsername.text = courseData.courseName
+            }
+        }
+    }
+    @SuppressLint("NotifyDataSetChanged")
+    fun setList(courseData: CourseData) {
+        list.clear()
+        list.addAll(listOf(courseData))
+        notifyDataSetChanged()
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemRowCourseListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list[position])
+        holder.itemView.setOnClickListener {
+            onItemClickCallback?.onItemClicked(list[holder.bindingAdapterPosition])
+        }
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: CourseData)
+    }
+
+
+}
+
+
