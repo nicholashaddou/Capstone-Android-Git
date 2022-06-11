7d582604a344c0043a613322fe29dac64824ac99
Lukmanul Hakim <jurpro.online@gmail.com>
2022-06-11 23:55:27 +0700

    Menambahkan Support Agent Activity
    Menambahkan Support Agent Activity

---
+++ app/src/main/java/com/c22ps175/playlab/ui/support/SupportAgentActivity.kt
@@ -0,0 +1,41 @@
+package com.c22ps175.playlab.ui.support
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.c22ps175.playlab.R
+import com.c22ps175.playlab.databinding.ActivitySupportAgentBinding
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+class SupportAgentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySupportAgentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySupportAgentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.profile -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
