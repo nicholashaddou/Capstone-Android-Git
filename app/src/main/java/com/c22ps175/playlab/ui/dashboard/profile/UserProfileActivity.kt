4f948ed7e201c8d5bef0341969f6ae8cecef1ff6
Lukmanul Hakim <jurpro.online@gmail.com>
2022-06-12 00:02:33 +0700

    Menambahkan intent keSupport Agent Activity
    Menambahkan intent keSupport Agent Activity

--- app/src/main/java/com/c22ps175/playlab/ui/dashboard/profile/UserProfileActivity.kt
+++ app/src/main/java/com/c22ps175/playlab/ui/dashboard/profile/UserProfileActivity.kt
@@ -102,6 +102,9 @@ class UserProfileActivity : AppCompatActivity() {
         btnSupport.setOnClickListener {
             val fab: View = findViewById(R.id.fab_support)
             fab.setOnClickListener { view ->
+                val moveIntent = Intent(this@UserProfileActivity, SupportAgentActivity::class.java)
+                startActivity(moveIntent)
+
                 Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
                     .setAction("Action", null)
                     .show()
